745c37faa162b2e96d8f5384e222638fdb3352e9
srlomeli <143758217+srlomeli@users.noreply.github.com>
2024-09-24 12:42:56 -0700

    Check Guess Validity

--- Wordle.py
+++ Wordle.py
@@ -1,13 +1,13 @@
 ########################################
-# Name:
+# Name: Salvador Lom
 # Collaborators (if any):
 # GenAI Transcript (if any):
 # Estimated time spent (hr):
 # Description of any added extensions:
 ########################################
 
-from WordleGraphics import *  # WordleGWindow, N_ROWS, N_COLS, CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR, UNKNOWN_COLOR
-from english import * # ENGLISH_WORDS, is_english_word
+from WordleGraphics import WordleGWindow, N_ROWS, N_COLS, CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR, UNKNOWN_COLOR
+from english import ENGLISH_WORDS, is_english_word
 import random
 
 def wordle():
@@ -15,7 +15,16 @@ def wordle():
 
     def enter_action():
         # What should happen when RETURN/ENTER is pressed.
-        gw.show_message("You need to implement this method")
+        word_guessed = ""
+        for i in range(N_COLS):
+            letter = gw.get_square_letter(gw.get_current_row(), i)
+            word_guessed += letter
+
+        if word_guessed.lower() in ENGLISH_WORDS:
+            gw.show_message("Great guess!")
+        else:
+             gw.show_message("Not in word list!")
+
 
 
     gw = WordleGWindow()
